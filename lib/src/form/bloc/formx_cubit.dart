282e15dbf4563ac4734b6c11945ef0041fe57856
Cesar Castro <cesarcastrocesar@gmail.com>
2023-08-24 18:11:53 -0300

    Refactor FormXCubit (wip)

--- lib/src/form/bloc/formx_cubit.dart
+++ lib/src/form/bloc/formx_cubit.dart
@@ -1,17 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_formx/src/form/formx_field.dart';
 import 'package:flutter_formx/src/form/formx_interface.dart';
+import 'package:flutter_formx/src/form/formx_state.dart';
 import 'package:flutter_formx/src/form/vanilla/formx.dart';
 
 /// Bloc implementation of [FormX]
-class FormXCubit<T> extends Cubit<FormX<T>> implements FormXInterface<T> {
+class FormXCubit<T> extends Cubit<FormXState<T>> implements FormXInterface<T> {
   /// When FormXCubit is instantiated, it emits the initial state of the form.
-  FormXCubit() : super(const FormX());
+  FormXCubit() : super(const FormXState({}));
+
+  FormX<T> _formX = FormX<T>();
 
   /// Bloc implementation of [FormX.setupForm].
   @override
   Future<void> setupForm(Map<T, FormXField> inputs) {
-    emit(FormX<T>(inputs));
+    emit(FormXState<T>(inputs));
+    _formX = FormX(inputs);
     return validateForm(softValidation: true);
   }
 
@@ -23,22 +27,25 @@ class FormXCubit<T> extends Cubit<FormX<T>> implements FormXInterface<T> {
     bool softValidation = false,
   }) async {
     final newState =
-        await state.updateAndValidateField(newValue, type, softValidation: softValidation);
-    emit(newState);
+        await _formX.updateAndValidateField(newValue, type, softValidation: softValidation);
+    _formX = newState;
+    emit(_formX.state);
   }
 
   /// Bloc implementation of [FormX.updateField].
   @override
   void updateField(dynamic newValue, T type) {
-    final newState = state.updateField(newValue, type);
-    emit(newState);
+    final newState = _formX.updateField(newValue, type);
+    _formX = newState;
+    emit(_formX.state);
   }
 
   /// Bloc implementation of [FormX.validateForm].
   @override
   Future<bool> validateForm({bool softValidation = false}) async {
-    final newState = await state.validateForm(softValidation: softValidation);
-    emit(newState);
+    final newState = await _formX.validateForm(softValidation: softValidation);
+    _formX = newState;
+    emit(_formX.state);
     return state.isFormValid;
   }
 }
